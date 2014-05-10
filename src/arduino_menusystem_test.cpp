7bce83de20d515d670e67448f017631a8b44c972
Dieter Niklaus <dieter.niklaus@gmx.net>
2014-05-10 16:04:27 +0200

    - NEW: implemented the Menu System Test application
    Signed-off-by: Dieter Niklaus <dieter.niklaus@gmx.net>

--- src/arduino_menusystem_test.cpp
+++ src/arduino_menusystem_test.cpp
@@ -1,16 +1,60 @@
 // Do not remove the include below
 #include "arduino_menusystem_test.h"
 
+#include "MenuHandler.h"
+#include "TimerContext.h"
 
+MenuHandler* menuHandler = 0;
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+  //---------------------------------------------------------------------------
+  // Debugging
+  //---------------------------------------------------------------------------
+  Serial.begin(115200);
+  Serial.println("-----------------------------------------------------------------");
+  Serial.println("Hello from Arduino MenuSystem Test!");
+  Serial.println("-----------------------------------------------------------------");
+  Serial.println("Press 'l' or 'a' for left, 'r' or 'd' for right, 's' for select");
+  Serial.println("-----------------------------------------------------------------\n");
+
+  //---------------------------------------------------------------------------
+  // MenuHandler
+  //---------------------------------------------------------------------------
+  menuHandler = new MenuHandler();
+  menuHandler->init();
+}
+
+/*
+ SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    if ('s' == inChar)
+    {
+      menuHandler->selectKeyEvent();
+    }
+    else if (('l' == inChar) || ('a' == inChar))
+    {
+      menuHandler->leftKeyEvent();
+    }
+    else if (('r' == inChar) || ('d' == inChar))
+    {
+      menuHandler->rightKeyEvent();
+    }
+  }
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+  TimerContext::instance()->handleTick();
 }

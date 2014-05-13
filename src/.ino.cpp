b75274cd0d19d788a0acd304df1121c1260c7b34
Dieter Niklaus <dieter.niklaus@gmx.net>
2014-05-13 03:33:45 +0200

    - FIX: removed unused and changing file src/.ino.cpp

--- src/.ino.cpp
+++
@@ -1,7 +0,0 @@
-//This is a automatic generated file
-//Please do not modify this file
-//If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2014-05-13 03:14:45
-
-#include "Arduino.h"
-

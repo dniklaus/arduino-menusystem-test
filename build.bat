ca712ca80652612f4bcc39515f2415411e44fc8c
Dieter Niklaus <dieter.niklaus@gmx.net>
2014-06-06 10:35:13 +0200

    - NEW: added build.bat (headless build)
    Signed-off-by: Dieter Niklaus <dieter.niklaus@gmx.net>

---
+++ build.bat
@@ -0,0 +1,9 @@
+::-----------------------------------------------------------------------------
+:: Paths
+::-----------------------------------------------------------------------------
+set SCRIPT_DIR=%~dp0%
+set ProjectHome=%SCRIPT_DIR%
+set WorkspaceDir=%ProjectHome%\workspace
+set ThisProjTools=%ProjectHome%\tools
+
+%ThisProjTools%\eclipseArduino\eclipsec.exe -data %WorkspaceDir% -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -cleanBuild all -vmargs -Dorg.eclipse.cdt.core.console=org.eclipse.cdt.core.systemConsole
